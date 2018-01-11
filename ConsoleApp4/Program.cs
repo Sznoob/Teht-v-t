c08e2db9555946459f64a1d811db4171248a4c9e
L7043 <L7043@OHJE-330-WS-21>
2018-01-11 17:10:31 +0200

    Tasks

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -10,6 +10,56 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
+            //task 1
+
+            Console.WriteLine("Give a number between 1-3: ");
+            int numb = Convert.ToInt16(Console.ReadLine());
+
+            if (numb == 1)
+                Console.WriteLine("Number is one");
+            else if (numb == 2)
+                Console.WriteLine("Number is two");
+            else if (numb == 3)
+                Console.WriteLine("Number is three");
+            else if (numb >= 4)
+                Console.WriteLine("Wrong number");
+            else
+            {
+                return;
+            }
+
+
+
+            //task 2
+
+
+            Console.WriteLine("Give point limit: ");
+            int point = Convert.ToInt16(Console.ReadLine());
+
+            if (point <= 1)
+                Console.WriteLine("Your grade is 0");
+            else if (point <= 3)
+                Console.WriteLine("Your grade is 1");
+            else if (point <= 5)
+                Console.WriteLine("Your grade is 2");
+            else if (point <= 7)
+                Console.WriteLine("Your grade is 3");
+            else if (point <= 9)
+                Console.WriteLine("Your grade is 4");
+            else if (point <= 12)
+                Console.WriteLine("Your grade is 5, " + ("Well done!"));
+
+
+            //task 3
+
+            Console.WriteLine("Give a first number ");
+
+
+
+
+
+
+            Console.Read();
         }
     }
 }
